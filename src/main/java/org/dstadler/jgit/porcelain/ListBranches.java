3fb7a1887b2fe9b17358a3e89f034a062cfba605
Dominik Stadler <dominik.stadler@gmx.at>
2013-06-17 19:03:08 +0200

    Add ListBranches

---
+++ src/main/java/org/dstadler/jgit/porcelain/ListBranches.java
@@ -0,0 +1,31 @@
+package org.dstadler.jgit.porcelain;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.dstadler.jgit.helper.CookbookHelper;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+
+
+
+/**
+ * Simple snippet which shows how to list all Branches in a Git repository
+ *
+ * @author dominik.stadler@gmx.at
+ */
+public class ListBranches {
+
+	public static void main(String[] args) throws IOException, GitAPIException {
+		Repository repository = CookbookHelper.openJGitCookbookRepository();
+
+		List<Ref> call = new Git(repository).branchList().call();
+		for(Ref rev : call) {
+			System.out.println("Branch: " + rev + " " + rev.getName() + " " + rev.getObjectId().getName());
+		}
+
+		repository.close();
+	}
+}

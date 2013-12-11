b58a1865e5cb27c59d478fb0940bf172dc18ab90
Peter LaValle <peter.lavalle@gmail.com>
2013-12-11 17:24:49 +0000

    Update CommitFile.java

--- src/main/java/org/dstadler/jgit/porcelain/CommitFile.java
+++ src/main/java/org/dstadler/jgit/porcelain/CommitFile.java
@@ -11,7 +11,7 @@ import org.eclipse.jgit.lib.Repository;
 
 
 /**
- * Simple snippet which shows how to list all Tags
+ * Simple snippet which shows how to commit a file
  * 
  * @author dominik.stadler@gmx.at
  */

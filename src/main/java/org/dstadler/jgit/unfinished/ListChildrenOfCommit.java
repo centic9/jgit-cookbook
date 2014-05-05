579254e864ebc2f7cc8ca06ab0feea66db901e06
Dominik Stadler <dominik.stadler@gmx.at>
2014-05-05 20:46:03 +0200

    Define Apache 2.0 license in README.md and source files

--- src/main/java/org/dstadler/jgit/unfinished/ListChildrenOfCommit.java
+++ src/main/java/org/dstadler/jgit/unfinished/ListChildrenOfCommit.java
@@ -1,5 +1,21 @@
 package org.dstadler.jgit.unfinished;
 
+/*
+   Copyright 2013, 2014 Dominik Stadler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import java.io.IOException;
 
 import org.dstadler.jgit.helper.CookbookHelper;
@@ -14,7 +30,7 @@ import org.eclipse.jgit.revwalk.RevCommit;
 /**
  * Snippet which shows how to use PlotWalk to read from a specific commit.
  * 
- * @author dominik.stadler@gmx.at
+ * @author dominik.stadler at gmx.at
  */
 public class ListChildrenOfCommit {
 
--- src/main/java/org/dstadler/jgit/unfinished/TrackMaster.java
+++ src/main/java/org/dstadler/jgit/unfinished/TrackMaster.java
@@ -1,5 +1,21 @@
 package org.dstadler.jgit.unfinished;
 
+/*
+   Copyright 2013, 2014 Dominik Stadler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import java.io.File;
 import java.io.IOException;
 
@@ -15,7 +31,7 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 /**
  * Simple snippet which shows how to clone a repository from a remote source
  * 
- * @author dominik.stadler@gmx.at
+ * @author dominik.stadler at gmx.at
  */
 public class TrackMaster {
 

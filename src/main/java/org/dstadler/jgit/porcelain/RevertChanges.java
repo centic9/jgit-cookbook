b771e380a6f9a6ba114e18bf2ad61924c137b3e5
Jordan Martinez <jordanalex.martinez@gmail.com>
2016-03-21 15:44:41 -0700

    Fix typo in error message

--- src/main/java/org/dstadler/jgit/porcelain/RevertChanges.java
+++ src/main/java/org/dstadler/jgit/porcelain/RevertChanges.java
@@ -58,7 +58,7 @@ public class RevertChanges {
 
                 // text should no longer have modifications
                 if (!initialText.equals(getTextFromFilePath(tempFilePath))) {
-                    throw new IllegalStateException("Reverted file's text should equal to its initial text");
+                    throw new IllegalStateException("Reverted file's text should equal its initial text");
                 }
 
                 System.out.println("File modifications were reverted. " +

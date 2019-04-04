188b4cf4222428fa9a10e5709675e88686127397
Dominik Stadler <dominik.stadler@dynatrace.com>
2019-04-04 13:23:34 +0200

    Reformat and update comment of new snippet

--- src/main/java/org/dstadler/jgit/porcelain/FetchRemoteCommitsWithSshAuth.java
+++ src/main/java/org/dstadler/jgit/porcelain/FetchRemoteCommitsWithSshAuth.java
@@ -20,28 +20,28 @@ import java.util.Map;
 /**
  * Simple snippet which shows how to fetch commits from a remote Git repository
  * via ssh protocol authentication.
+ *
+ * You need to provide proper values for the URL of the Git repository and
+ * the location of the private key file
+ *
  * <p>
  * Created by zhengmaoshao on 2019/4/3 上午12:38
  */
-public class FetchRemoteCommitsWithSshAuth
-{
-
+public class FetchRemoteCommitsWithSshAuth {
     private static final String REMOTE_URL = "ssh://<user>@<host>:22/<path-to-remote-repo>/";
+    private static final String PRIVATE_KEY = "/path/to/private_key";
 
     public static void main(String[] args) throws GitAPIException {
 
-        final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory()
-        {
+        final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
             @Override
             protected void configure(OpenSshConfig.Host host, Session session) {
-
             }
 
             @Override
             protected JSch createDefaultJSch(FS fs) throws JSchException {
-
                 JSch defaultJSch = super.createDefaultJSch(fs);
-                defaultJSch.addIdentity("/path/to/private_key");
+                defaultJSch.addIdentity(PRIVATE_KEY);
                 return defaultJSch;
             }
         };
@@ -64,6 +64,5 @@ public class FetchRemoteCommitsWithSshAuth
         for (Map.Entry<String, Ref> entry : map.entrySet()) {
             System.out.println("Key: " + entry.getKey()/*eg.refs/heads/develop*/ + ", Ref: " + entry.getValue().getObjectId().getName()/*eg.e16c937848d5c1ad50ef163003c7b076103f7e37*/);
         }
-
     }
 }

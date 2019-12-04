895bb2bfc0523bac6baec91f92455d4f2cc20bb1
Dominik Stadler <dominik.stadler@gmx.at>
2019-12-04 20:19:29 +0100

    Add comment of how to limit commits to only one branch

--- src/main/java/org/dstadler/jgit/porcelain/WalkAllCommits.java
+++ src/main/java/org/dstadler/jgit/porcelain/WalkAllCommits.java
@@ -24,8 +24,6 @@ import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.io.IOException;
 
-
-
 /**
  * Simple snippet which shows how to use RevWalk to quickly iterate over all available commits,
  * not just the ones on the current branch
@@ -35,6 +33,10 @@ public class WalkAllCommits {
     public static void main(String[] args) throws IOException, GitAPIException {
         try (Repository repository = CookbookHelper.openJGitCookbookRepository()) {
             try (Git git = new Git(repository)) {
+                // use the following instead to list commits on a specific branch
+                //ObjectId branchId = repository.resolve("HEAD");
+                //Iterable<RevCommit> commits = git.log().add(branchId).call();
+
                 Iterable<RevCommit> commits = git.log().all().call();
                 int count = 0;
                 for (RevCommit commit : commits) {

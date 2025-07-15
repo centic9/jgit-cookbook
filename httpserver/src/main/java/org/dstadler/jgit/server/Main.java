d20bd9bbf8c86dc2c2747e80739d974c4c165111
Dominik Stadler <dominik.stadler@gmx.at>
2025-07-15 08:02:44 +0200

    Convert to Jetty 12, but httpserver seems to not work...

--- httpserver/src/main/java/org/dstadler/jgit/server/Main.java
+++ httpserver/src/main/java/org/dstadler/jgit/server/Main.java
@@ -1,8 +1,7 @@
 package org.dstadler.jgit.server;
 
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.http.server.GitServlet;
@@ -55,12 +54,11 @@ public class Main {
     private static Server configureAndStartHttpServer(GitServlet gs) throws Exception {
         Server server = new Server(8080);
 
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
 
-        ServletHolder holder = new ServletHolder(gs);
-
-        handler.addServletWithMapping(holder, "/*");
+        context.addServlet(gs, "/repo");
 
         server.start();
         return server;
